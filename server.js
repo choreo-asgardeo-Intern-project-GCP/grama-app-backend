9ea35449077370a3f9260796b89758af24995ce4
odhithSen <odhith.sen@gmail.com>
2022-08-19 16:37:17 +0530

    Add initial file structure

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
-var http = require("http");
+//import dependancies
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import routes from "./routes/api_routes.js";
 
-http.createServer(function (request, response) {
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end('Auto deployment works! 2\n');
-}).listen(8081);
 
-console.log('Server running at http://127.0.0.1:8081/');
+const app = express ();
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+
+// should make this an .env variyable
+const CONNECTION_URL = "mongodb+srv://odhithSen:bVGaMLuIj5n5SZ5w@cluster0.0p0tv.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+// mongoose.connect(CONNECTION_URL, { useUnifiedTopology: true})
+// .then(() => app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
+// .catch((error)=> console.log(error.message));
+
+
+app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`))
+app.use("/", routes)
