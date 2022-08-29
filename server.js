17c5f43cda163adf5c59b876afa2e188aa57b8b2
Kumuditha Dodanwela <86454991+KD23243@users.noreply.github.com>
2022-08-29 19:00:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //import dependancies
 import express from "express";
 import bodyParser from "body-parser";
-// import cors from "cors";
+import cors from "cors";
 import routes from "./routes/api-routes.js";
 import mongoose from "mongoose";
 import dotenv  from "dotenv";
@@ -10,6 +10,8 @@ dotenv.config();
 const app = express ();
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+
+app.use(cors())
 
 app.use(function (req, res, next) {
 
