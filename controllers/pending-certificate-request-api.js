3e67335ebca0e7fd35de2d26eb5c446d59a96af0
Kumuditha Dodanwela <86454991+KD23243@users.noreply.github.com>
2022-08-29 18:55:35 +0530

    Update pending-certificate-request-api.js

--- controllers/pending-certificate-request-api.js
+++ controllers/pending-certificate-request-api.js
@@ -25,6 +25,18 @@ export const pending_certificate_request_api = async(req, res) =>{
 
                   filterd_certificate_data.push(user_data);
                 });
+                res.setHeader('Access-Control-Allow-Origin', '*');
+
+                // Request methods you wish to allow
+                res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+                // Request headers you wish to allow
+                res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+                // Set to true if you need the website to include cookies in the requests sent
+                // to the API (e.g. in case you use sessions)
+                res.setHeader('Access-Control-Allow-Credentials', true);
+
                 res.status(200).json({status: "success", filterd_certificate_data});
 
             } else{
