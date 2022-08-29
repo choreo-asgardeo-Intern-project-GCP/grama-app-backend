0eadf4fcd3e7a83cd6838ef22648f6bd6a692aa1
Kumuditha Dodanwela <86454991+KD23243@users.noreply.github.com>
2022-08-29 12:31:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express ();
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+app.options('*', cors())
 
 
 const connectionUrl = process.env.CONNECTION_URL; 
