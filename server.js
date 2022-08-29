2dd7546af4c19cc6d5ae68b3b6b3d0d7ca1a815d
Kumuditha Dodanwela <86454991+KD23243@users.noreply.github.com>
2022-08-29 12:44:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ dotenv.config();
 const app = express ();
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+
+app.use(function (req, res, next) {
+//Enabling CORS
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next();
+});
+
 app.use(cors());
 app.options('*', cors())
 
