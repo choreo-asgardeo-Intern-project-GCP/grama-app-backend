dc9492b3dfdf4f5ca5d511c1b656f43c43f5cb84
Kumuditha Dodanwela <86454991+KD23243@users.noreply.github.com>
2022-08-30 09:02:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors())
 
 const connectionUrl = process.env.CONNECTION_URL; 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 mongoose.connect(connectionUrl, { useUnifiedTopology: true})
 .then(() => app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
