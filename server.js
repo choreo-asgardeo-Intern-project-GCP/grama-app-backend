ad2f59b9ad35b5b7df71c28e7394f175766204f7
Kumuditha Dodanwela <86454991+KD23243@users.noreply.github.com>
2022-09-01 09:44:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 
 app.use(cors({
-    origin: "*",
+    origin: "https://nimbuzz01.github.io",
 }))
 
 const connectionUrl = process.env.CONNECTION_URL; 
